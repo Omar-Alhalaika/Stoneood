ee043fc7629ce5bf7992c6d19c09195f63aca8d0
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-02-23 17:34:32 +0200

    adding JS and edit html img .. etc

---
+++ JS/dynamic-1st.js
@@ -0,0 +1,24 @@
+confirm ("Are You ready to start an OUTSTANDING EXPERIENCE! ");
+
+// to welcome the guest
+var UserName= prompt("Kindly enter your name") ;
+var age= prompt("How old are you dear?");
+
+if (age>=18) {alert("welcome here")    
+}else alert("you need your parents to continue!")
+
+
+
+//var User2= parseInt()
+// document.write('<h1>'+"Welcome " + UserName + " to Stoneood; the art world"+'</h1>');
+
+//alert ("Welcome " + UserName + " to Stoneood; the art world");
+
+document.getElementById('ToUser').innerText ="Welcome " + UserName + " to Stoneood the Art World";
+
+// this one for replacing html command
+document.get
+//document.getElementById('ToUser').innerHTML = "Welcome " + UserName + " to Stoneood the Art World";
+
+
+//document.getElementById('').style.background = "red";
