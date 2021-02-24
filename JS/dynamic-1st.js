6443e331d048d8f6edfae81f5c39ea613f3c1586
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-02-24 16:55:58 +0200

    the final edit for  relaxing colour tour

--- JS/dynamic-1st.js
+++ JS/dynamic-1st.js
@@ -1,30 +1,39 @@
-confirm ("Are You ready to start \n an OUTSTANDING EXPERIENCE! ");
+confirm ("Are You ready to start \nan OUTSTANDING EXPERIENCE! ");
 
 // to welcome the guest
 var UserName= prompt("Kindly enter your name") ;
 var age= prompt("How old are you dear?");
-var color.toLowerCase = prompt("Can you give me one of the most \n Relaxing Colors! ")
-
-
 if (age>=18) {alert("welcome here");  
-}else alert("you need your parents to continue!")
+}else alert("you need your parents to continue!");
 
+var color = prompt("Can you give me one of the most \n Relaxing Colors! ");
+function popup() {
+    if (color.toLowerCase() == "blue"||color.toLowerCase() == "violet"||color.toLowerCase() == "purple"||color.toLowerCase() == "green") {
+        alert("Great job! \n " + color + " is one of the most relaxing colors")
+        
+    } else { alert("Try again \nyou may think about sky, grass ..!")
+    //color = '';
+    color =prompt("Can you give me one of the most \n Relaxing Colors! ");
 
-color
-if (color=== "blue"||"violet"||"purple"||"green") {
-    alert("Great job! \n " + color + "is one of the most relaxing colors")
-    
-} else { alert("Try again \n you may think about sky, grass ..!")
+popup()
+};
     
 }
+console.log(color)
+
+popup()
+
 
 //var User2= parseInt()
 // document.write('<h1>'+"Welcome " + UserName + " to Stoneood; The world of art"+'</h1>');
 
-alert ("Welcome " + UserName + " to Stoneood \n The world of art ");
+alert ("Welcome " + UserName + " to Stoneood \nThe world of art ");
 
-document.getElementById('ToUser').innerText ="Welcome " + UserName + " to Stoneood The world of art";
-
+//function TextToUser(params) {
+    
+}
+document.getElementById('ToUser').innerText ="Welcome " + UserName + " to Stoneood ";
+document.getElementById(`AboutColor`).innerText = "( " + color.toUpperCase() + " )"+ " is a soothing color that can invite harmony and diffuse anxiety. \n and helpful for stress management because it can encourage a powerful sense of calm.";
 // this one for replacing html command
 //document.get
 //document.getElementById('ToUser').innerHTML = "Welcome " + UserName + " to Stoneood The world of art";
