3865730e33b72f28b1fe68623f2c90337967d7c3
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-02-24 15:23:38 +0200

    1st step for asking for the color

--- JS/dynamic-1st.js
+++ JS/dynamic-1st.js
@@ -1,24 +1,33 @@
-confirm ("Are You ready to start an OUTSTANDING EXPERIENCE! ");
+confirm ("Are You ready to start \n an OUTSTANDING EXPERIENCE! ");
 
 // to welcome the guest
 var UserName= prompt("Kindly enter your name") ;
 var age= prompt("How old are you dear?");
+var color.toLowerCase = prompt("Can you give me one of the most \n Relaxing Colors! ")
 
-if (age>=18) {alert("welcome here")    
+
+if (age>=18) {alert("welcome here");  
 }else alert("you need your parents to continue!")
 
 
+color
+if (color=== "blue"||"violet"||"purple"||"green") {
+    alert("Great job! \n " + color + "is one of the most relaxing colors")
+    
+} else { alert("Try again \n you may think about sky, grass ..!")
+    
+}
 
 //var User2= parseInt()
-// document.write('<h1>'+"Welcome " + UserName + " to Stoneood; the art world"+'</h1>');
+// document.write('<h1>'+"Welcome " + UserName + " to Stoneood; The world of art"+'</h1>');
 
-//alert ("Welcome " + UserName + " to Stoneood; the art world");
+alert ("Welcome " + UserName + " to Stoneood \n The world of art ");
 
-document.getElementById('ToUser').innerText ="Welcome " + UserName + " to Stoneood the Art World";
+document.getElementById('ToUser').innerText ="Welcome " + UserName + " to Stoneood The world of art";
 
 // this one for replacing html command
-document.get
-//document.getElementById('ToUser').innerHTML = "Welcome " + UserName + " to Stoneood the Art World";
+//document.get
+//document.getElementById('ToUser').innerHTML = "Welcome " + UserName + " to Stoneood The world of art";
 
 
 //document.getElementById('').style.background = "red";
